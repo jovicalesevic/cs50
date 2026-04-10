ebbf04213632a8967cf02c110cb6ec177c95a18c
Jovica Lesevic <jovica.lesevic@gmail.com>
2026-04-10 21:06:23 +0200

    Završen Mario sa validacijom unosa

---
+++ week1/mario.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+    //Tražimo od korisnika visinu - na primer od 1 do 8
+    do
+    {
+        n = get_int("Height (between 1 and 8): ");
+    }
+    while (n < 1 || n > 8);
+ 
+    // Spoljašnja petlja za redove
+    for (int i = 0; i < n; i++)
+    {
+        // Unutrašnja petlja za razmake
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            printf(" ");
+        }
+
+        // Unutrašnja petlja za tarabe
+        for (int k = 0; k <= i; k++)
+        {  
+            printf("#");
+        }
+            // Prelazak u novi red nakon što se iscrta jedna linija
+            printf("\n");
+    }
+}
